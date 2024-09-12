6e757660ce2b641a62baa43c1d2c4d2972f8f430
lets <wilgoncalves98@gmail.com>
2024-09-11 21:49:40 -0300

    Desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,12 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+        
+              //Suite suite = new Suite();
+
+            int capacidade = Suite.Capacidade; 
+
+            if (capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -37,7 +42,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int capacidade = Hospedes.Count;
+            return capacidade;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +51,14 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
 
+            decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = (DiasReservados * Suite.ValorDiaria) - DiasReservados / 100;
             }
 
             return valor;
